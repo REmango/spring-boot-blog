b436f9d3079f55f44814e0501364704b0ae90ede
remango <994857325@qq.com>
2019-09-05 15:04:05 +0800

    Update OSSClientConstants.java
    update

--- src/main/java/com/remango/blog/constants/OSSClientConstants.java
+++ src/main/java/com/remango/blog/constants/OSSClientConstants.java
@@ -7,16 +7,16 @@ public class OSSClientConstants {
     /**
      * 阿里云API的外网域名
      */
-    public static final String ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
+    public static final String ENDPOINT = "xxx";
 
     /**
      * 阿里云API的密钥Access Key ID
      */
-    public static final String ACCESS_KEY_ID = "LTAIBQunNTe2K7hE";
+    public static final String ACCESS_KEY_ID = "xxxx";
     /**
      *阿里云API的密钥Access Key Secret
      */
-    public static final String ACCESS_KEY_SECRET = "n30KIHGLM1qWdUmdpUoNEqTUjpWEOm";
+    public static final String ACCESS_KEY_SECRET = "xxx";
 
     /**
      * 阿里云API的bucket名称
